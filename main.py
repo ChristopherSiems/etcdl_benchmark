3a1c9bb86e3419fd7966dd9de18b163d36e30bb3
ChristopherSiems <christophersiems@protonmail.com>
2025-07-10 10:57:25 -0400

    next step

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from helpers import remote_execute
 
+ETCD_CLIENT_CMD: str = 'cd /local/etcd-client && git pull && go build && ./etcd-client -addresses=10.10.1.1:2379,10.10.1.2:2379,10.10.1.3:2379 -data-size=1 -num-ops=10000 -read-ratio=1.0 -num-clients=5'
+
 if __name__ == '__main__':
-    print(remote_execute('10.10.1.1',
-          'echo "hello world"').communicate()[0].strip())
+    remote_execute('10.10.1.1', 'cd /local && sh run_etcd0.sh')
+    remote_execute('10.10.1.2', 'cd /local && sh run_etcd1.sh')
+    remote_execute('10.10.1.3', 'cd /local && sh run_etcd2.sh')
+    print(remote_execute('10.10.1.4', ETCD_CLIENT_CMD).communicate[0].strip())

7e65c230cbfcd49118cba969355ff5f170dc92e1
ChristopherSiems <christophersiems@protonmail.com>
2025-07-10 10:58:55 -0400

    next step 2

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     remote_execute('10.10.1.1', 'cd /local && sh run_etcd0.sh')
     remote_execute('10.10.1.2', 'cd /local && sh run_etcd1.sh')
     remote_execute('10.10.1.3', 'cd /local && sh run_etcd2.sh')
-    print(remote_execute('10.10.1.4', ETCD_CLIENT_CMD).communicate[0].strip())
+    print(remote_execute('10.10.1.4',
+          ETCD_CLIENT_CMD).communicate()[0].strip())

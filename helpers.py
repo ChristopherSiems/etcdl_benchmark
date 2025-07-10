710f0a8d76147783da3004799327fe07b1d40eba
ChristopherSiems <christophersiems@protonmail.com>
2025-07-10 13:01:33 -0400

    setup all nodes

--- helpers.py
+++ helpers.py
@@ -12,13 +12,38 @@ def remote_execute(address: str, cmd: str) -> Popen:
     :rtype: Popen
     '''
 
+    print(f'$ {cmd}')
     return Popen(['sudo', 'ssh', '-o', 'StrictHostKeyChecking=no',
                   f'root@{address}', f'PATH=$PATH:/usr/local/go/bin && {cmd}'],
                  stdout=PIPE, stderr=STDOUT, text=True)
 
 
 def wait_output(process: Popen, target: str) -> None:
+    '''
+    wait for a specific output from a process
+    :param process: the running process
+    :type process: Popen
+    :param target: the output to look for
+    :type target: str
+    '''
+
+    print('waiting... ', end='')
     while True:
         if target in process.stdout.readline():
-            print('found')
+            print('continuing')
             return
+
+
+def execute_wait(address: str, cmd: str, target: str) -> None:
+    '''
+    execute a given command on an addressed computer, then wait for a specific
+    output from the process
+    :param address: the IP address of the computer to execute on
+    :type address: str
+    :param cmd: the command to execute
+    :type cmd: str
+    :param target: the output to look for
+    :type target: str
+    '''
+
+    wait_output(remote_execute(address, cmd), target)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from helpers import remote_execute, wait_output
-
-ETCD_CLIENT_CMD: str = 'cd /local/etcd-client && git pull && go build && ./etcd-client -addresses=10.10.1.1:2379,10.10.1.2:2379,10.10.1.3:2379 -data-size=1 -num-ops=10000 -read-ratio=1.0 -num-clients=5'
+from helpers import execute_wait
 
 if __name__ == '__main__':
-    wait_output(remote_execute(
-        '10.10.1.1', 'cd /local && sh run_etcd0.sh'),
-        'Starting etcd...')
+    execute_wait('10.10.1.1', 'cd /local && sh run_etcd0.sh',
+                 'Starting etcd...')
+    execute_wait('10.10.1.2', 'cd /local && sh run_etcd1.sh',
+                 'Starting etcd...')
+    execute_wait('10.10.1.3', 'cd /local && sh run_etcd2.sh',
+                 'Starting etcd...')

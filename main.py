7c41e3fd7e80a0b0600d6edad0d6374d2090d8df
ChristopherSiems <christophersiems@protonmail.com>
2025-07-14 11:30:12 -0400

    added sleep

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ from helpers import (config_get, exec_wait, extract_num, git_interact,
                      kill_servers, remote_exec_sync)
 
 ETCD_CLIENT_CMD: str = '(PATH=/usr/local/go/bin:$PATH && cd /local/etcd-client && git pull && go build && ./etcd-client -addresses={server_addrs} -data-size={data_size} -num-ops={num_operations} -read-ratio={read_ratio} -num-clients={num_clients})'
-ETCDL_SERVER_CMD: str = '(PATH=/usr/local/go/bin:$PATH && cd /local/go_networking_benchmark/run && cd .. && git fetch && git checkout dev && git pull && go build && mv networking_benchmark run/ && cd /local/go_networking_benchmark/run && ./networking_benchmark server -num-dbs={num_dbs} -max-db-index={db_indices} -node={node_num} -memory=false -wal-file-count={wal_file_count} -manual=fsync -flags=none -peer-connections=1 -peer-listen="{server_1}:6900" -client-listen="{server_2}:7000" -peer-addresses="{peer_addrs}" -fast-path-writes={fast_path_writes})'
+ETCDL_SERVER_CMD: str = '(PATH=/usr/local/go/bin:$PATH && cd /local/go_networking_benchmark/run && cd .. && git fetch && git checkout dev && git pull && go build && mv networking_benchmark run/ && cd /local/go_networking_benchmark/run && ./networking_benchmark server -num-dbs={num_dbs} -max-db-index={db_indices} -node={node_num} -memory=false -wal-file-count={wal_file_count} -manual=fsync -flags=none -peer-connections=1 -peer-listen="{server_1}:6900" -client-listen="{server_2}:7000" -peer-addresses="{peer_addrs}" -fast-path-writes=true)'
 ETCDL_CLIENT_CMD: str = '(PATH=/usr/local/go/bin:$PATH && cd /local/go_networking_benchmark && git fetch && git checkout dev && git pull && go build && ./networking_benchmark client -addresses={server_addrs} -data-size={data_size} -ops={num_operations} -read-ratio={read_ratio} -clients={num_clients} -read-mem=false -write-mem=false -find-leader=false)'
 
-CSV_HEADER: str = 'system,server_count,data_size,read_ratio,num_clients,num_dbs,wal_file_count,fast_path_writes,ops,med,p95,p99\n'
-CSV_ENTRY: str = '{system},{server_count},{data_size},{read_ratio},{num_clients},{num_dbs},{wal_file_count},{fast_path_writes},{ops},{med},{p95},{p99}\n'
+CSV_HEADER: str = 'system,server_count,data_size,read_ratio,num_clients,num_dbs,wal_file_count,ops,med,p95,p99\n'
+CSV_ENTRY: str = '{system},{server_count},{data_size},{read_ratio},{num_clients},{num_dbs},{wal_file_count},{ops},{med},{p95},{p99}\n'
 
 OPS_PATTERN: Pattern = rcompile(r' OPS\(\d+\) ')
 MED_PATTERN: Pattern = rcompile(r' 50th\(\d+\) ')
@@ -59,7 +59,6 @@ if __name__ == '__main__':
 
             num_dbs: int = config_get(cfg, 'num_dbs')
             wal_file_count: int = config_get(cfg, 'wal_file_count')
-            fast_path_writes: str = config_get(cfg, 'fast_path_writes')
 
             data_filepath: str = f'data/{test_name}.csv'
             addrs: str = ':{port_num},'.join(servers) + ':{port_num}'
@@ -78,8 +77,7 @@ if __name__ == '__main__':
                                                            server_1=server,
                                                            server_2=server,
                                                            peer_addrs=addrs.format(
-                                                               port_num=6900),
-                                                           fast_path_writes=fast_path_writes)
+                                                               port_num=6900))
                 processes.append(
                     exec_wait(server, server_cmd_fmt, server_target))
 
@@ -120,7 +118,6 @@ if __name__ == '__main__':
                                                      num_clients=num_clients,
                                                      num_dbs=num_dbs,
                                                      wal_file_count=wal_file_count,
-                                                     fast_path_writes=fast_path_writes,
                                                      ops=ops,
                                                      med=med,
                                                      p95=p95,
